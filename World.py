58dd59bd091d1030a88669c8a4e49682b0f1f4e7
houhashv <yossihohashvili1@gmail.com>
2019-06-15 23:35:47 +0300

    checks

--- World.py
+++ World.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class World:
@@ -8,7 +8,84 @@ class World:
 
         self.nRows = 3
         self.nCols = 4
-        self.stateObstacles = 5
+        self.stateObstacles = [5]
         self.stateTerminals = [10, 11]
         self.nStates = 12
         self.nActions = 4
+
+    def _plot_world(self):
+
+        nStates = self.nStates
+        nRows = self.nRows
+        nCols = self.nCols
+        stateObstacles = self.stateObstacles
+        stateTerminals = self.stateTerminals
+        coord = [[0, 0], [nCols, 0], [nCols, nRows], [0, nRows], [0, 0]]
+        xs, ys = zip(*coord)
+        plt.plot(xs, ys, "black")
+        for i in stateObstacles:
+            (I, J) = np.unravel_index(i, shape=(nRows, nCols), order='F')
+            coord = [[J, nRows - I],
+                     [J + 1, nRows - I],
+                     [J + 1, nRows - I + 1],
+                     [J, nRows - I + 1],
+                     [J, nRows - I]]
+            xs, ys = zip(*coord)
+            plt.fill(xs, ys, "0.5")
+            plt.plot(xs, ys, "black")
+        for ind, i in enumerate(stateTerminals):
+            (I, J) = np.unravel_index(i, shape=(nRows, nCols), order='F')
+            coord = [[J, nRows - I],
+                     [J + 1, nRows - I],
+                     [J + 1, nRows - I + 1],
+                     [J, nRows - I + 1],
+                     [J, nRows - I]]
+            xs, ys = zip(*coord)
+            plt.fill(xs, ys, "0.8")
+            plt.plot(xs, ys, "black")
+        plt.plot(xs, ys, "black")
+        X, Y = np.meshgrid(range(nCols + 1), range(nRows + 1))
+        plt.plot(X, Y, 'k-')
+        plt.plot(X.transpose(), Y.transpose(), 'k-')
+
+    @staticmethod
+    def _truncate(n, decimals=0):
+        multiplier = 10 ** decimals
+        return int(n * multiplier) / multiplier
+
+    def plot(self):
+        """
+        plot function
+        :return: None
+        """
+        nStates = self.nStates
+        nRows = self.nRows
+        nCols = self.nCols
+        self._plot_world()
+        states = range(1, nStates + 1)
+        k = 0
+        for i in range(nCols):
+            for j in range(nRows, 0, -1):
+                plt.text(i + 0.5, j - 0.5, str(states[k]), fontsize=26, horizontalalignment='center', verticalalignment='center')
+                k += 1
+        plt.title('MDP gridworld', size=16)
+        plt.axis("equal")
+        plt.axis("off")
+        plt.show()
+
+    def plot_value(self, valueFunction):
+
+        nRows = self.nRows
+        nCols = self.nCols
+        stateObstacles = self.stateObstacles
+        self._plot_world()
+        k = 0
+        for i in range(nCols):
+            for j in range(nRows, 0, -1):
+                if k + 1 not in stateObstacles:
+                    plt.text(i + 0.5, j - 0.5, str(self._truncate(valueFunction[k], 3)), fontsize=26, horizontalalignment='center', verticalalignment='center')
+                k += 1
+        plt.title('MDP gridworld', size=16)
+        plt.axis("equal")
+        plt.axis("off")
+        plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from World import World
+import numpy as np
 
 
 if __name__ == "__main__":
 
     world = World()
+    world.plot()
+    world.plot_value([np.random.random() for i in range(12)])
 
